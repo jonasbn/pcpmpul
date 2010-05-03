fc1f8b867fad48968dcc379daee9a61a1c81632a
jonasbn <jonasbn@logiclab.dk>
2010-05-03 08:54:33 +0000

    Added POD and fixed a minor text error
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/PCPMPUL/trunk@7061 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Perl/Critic/Policy/Modules/ProhibitUseLib.pm
+++ lib/Perl/Critic/Policy/Modules/ProhibitUseLib.pm
@@ -10,7 +10,7 @@ use version;
 
 our $VERSION = '0.01';
 
-Readonly::Scalar my $EXPL => q{Use PERL5INC environment instead};
+Readonly::Scalar my $EXPL => q{Use PERL5LIB environment instead};
 
 use constant supported_parameters => ();
 use constant default_severity     => $SEVERITY_MEDIUM;
@@ -36,3 +36,86 @@ sub violates {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Perl::Critic::Policy::Modules::ProhibitUseLib
+
+=head1 VERSION
+
+This documentation describes version 0.01
+
+=head1 DESCRIPTION
+
+The 'use lib' statement, hardcodes the include path to be used. This can give
+issues when moving modules and scripts between diverse environments.
+
+    use lib '/some/path';                                       #not ok
+    use lib qw(/you/do/not/want/to/go/down/this/path /or/this); #not ok
+
+Instead use the environment variable PERL5LIB
+
+    #bash
+    export PERL5LIB='/some/path/some/where'
+    
+    #tcsh and csh
+    setenv PERL5LIB '/some/path/some/where'
+
+=head1 AFFILIATION
+
+This policy is part of L<Perl::Critic::JONASBN> distribution.
+    
+=head1 CONFIGURATION
+
+This Policy is not configurable except for the standard options.
+    
+=head1 DEPENDENCIES AND REQUIREMENTS
+
+Please see the specific policies.
+
+=head1 INCOMPATIBILITIES
+
+No known incompatibilities.
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 BUG REPORTING
+
+=head1 TEST AND QUALITY
+
+=head2 TEST COVERAGE
+
+    ---------------------------- ------ ------ ------ ------ ------ ------ ------
+    File                           stmt   bran   cond    sub    pod   time  total
+    ---------------------------- ------ ------ ------ ------ ------ ------ ------
+    ...Modules/ProhibitUseLib.pm  100.0  100.0    n/a  100.0  100.0  100.0  100.0
+    Total                         100.0  100.0    n/a  100.0  100.0  100.0  100.0
+    ---------------------------- ------ ------ ------ ------ ------ ------ ------
+
+=head1 TODO
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<http://perldoc.perl.org/perlrun.html#ENVIRONMENT>
+
+=item * L<http://logiclab.jira.com/wiki/display/OPEN/Development#Development-MakeyourComponentsEnvironmentAgnostic>
+
+=back
+
+=head1 AUTHOR
+
+=over
+
+=item * Jonas B. Nielsen, jonasbn C<< <jonasbn@cpan.org> >>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+=cut
